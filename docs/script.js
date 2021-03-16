691a6819371a7555bb1afd4abd9dc4e9e96bd5bb
ChapelR <Mason_718@hotmail.com>
2021-03-16 05:23:39 -0400

    improved recs

--- docs/script.js
+++ docs/script.js
@@ -3,9 +3,20 @@
     "use strict";
 
     function calcScore () {
-        let score = 0;
+        let score = 0, addLater = 0, alwaysRec = false;
         $("select").each((i, el) => {
-            score += Number($(el).val());
+            let val = $(el).val();
+            if (val === "special") {
+                addLater += 1;
+            } else if (val === "always-recommend") {
+                alwaysRec = true;
+            } else {
+                val = Number(val);
+                if (Number.isNaN(val)) {
+                    val = 0;
+                }
+                score += val;
+            }
         });
         if (score < 3 && $('select#format').val() === '0' && $('select#highlighting').val() === '2') {
             // not harlowe, req'd syntax highlighting, always recommend Tweego
@@ -13,6 +24,12 @@
         } else if ($('select#format').val() === '1' && $('select#highlighting').val() === '2') {
             // is harlowe, syntax highlighting = no points ever
             score -= 1;
+        }
+        if (alwaysRec && score < 3) {
+            score = 3;
+        }
+        if (addLater > 0 && score >= 3) {
+            score += addLater;
         }
         return score;
     }
